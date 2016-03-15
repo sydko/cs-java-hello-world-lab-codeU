a8378a5f4f940ebb73ff112102e7b5b6d1b7f6a9
Sydney Ko <kosydney@uchicago.edu>
2016-03-15 15:08:34 -0500

    yay, tests passed

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,11 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+	String versionString = System.getProperty("java.specification.version");
+	return Double.parseDouble(versionString);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	System.out.println(getVersion());
     }
 }
